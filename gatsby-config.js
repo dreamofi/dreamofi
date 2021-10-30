10a881c4a701fd82169166662f2463befffc270e
Dreamofi <dxt.tfg@gmail.com>
2021-10-30 13:51:43 +0700

    Update gatsby-config.js, package.json, and yarn.lock

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,14 +62,10 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          {
-            family: `Montserrat`,
-            variants: [`400`, `700`, `900`],
-          },
-        ],
+        fonts: [`montserrat:400,700,900`],
+        display: "swap",
       },
     },
     {
